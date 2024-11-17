f59e14a464e30ef02704501143a48853094469c9
sx.ng <sx.ng@stratusauto.com>
2024-11-17 11:13:58 +0800

    Complete Login and Register page with firebase

--- chat-app/src/Pages/Register.js
+++ chat-app/src/Pages/Register.js
@@ -1,38 +1,102 @@
-import { UserOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Carousel } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import imgSrc from "../asset/images/login.jpg";
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { db, auth } from "../firebase/firebase-config";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [error, setError] = useState(null);
+
+    const signUp = async (values) => {
+
+        try {
+            if(values.confirmPassword !== values.password){
+                setError("Password do not match");        
+                return;
+            }
+
+            const userCredentials = await createUserWithEmailAndPassword(auth, values.email, values.password);
+            console.log(userCredentials.user);
+            
+            setError(null);
+            navigate("/");
+        } catch (error) {
+            setError(error.message)
+        }
+        
+    }
 
     return(
-        <div className='loginForm grid grid-cols-7 vanRegular'>
+        <div className='loginForm grid grid-cols-7'>
             <div className='login-details col-span-3'>
                 <div className='pb-14'>
-                    <h1 className='vanBold title text-3xl'>Chat App</h1>
-                    <p className='text-gray-400'>Welcome Back, Please login <br/> to your account</p>
+                    <h1 className='title text-3xl pb-2'>Get Started</h1>
+                    <p className='text-gray-400'>Create your account now</p>
                 </div>
-                <Form>
+                <Form onFinish={signUp}>
                     <Form.Item
-                    name="email"
-                    rules={[
-                        {
-                        required: true,
-                        message: 'Please enter your Email!',
-                        },
-                    ]}
+                        name="email"
+                        rules={[
+                            {
+                            required: true,
+                            message: 'Please enter your Email!',
+                            },
+                        ]}
+                        className='pb-4'
                     >
-                    <Input prefix={<UserOutlined />} placeholder="Email Address" />
+                    <Input className='p-3' placeholder="Email Address" />
+                    </Form.Item>
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter your Username!'
+                            },
+                        ]}
+                        className='pb-4'
+                    >
+                    <Input className='p-3' placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                            required: true,
+                            message: 'Please input your Password!',
+                            },
+                        ]}
+                        className='pb-4'
+                    >
+                    <Input className='p-3' type="password" placeholder="Password" />
+                    </Form.Item>
+                    <Form.Item
+                        name="confirmPassword"
+                        rules={[
+                            {
+                            required: true,
+                            message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                    <Input className='p-3' type="password" placeholder="Confirm Password" />
+                    {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
                     </Form.Item>
                     <Form.Item>
-                    <Button type='primary' flex htmlType='submit'>
+                    <Button className='login-btn mt-6' type='primary' flex htmlType='submit' onClick={signUp}>
                         Create Account
                     </Button>
                     </Form.Item>
+                    <div className='pt-14'>
+                        <span className='text-gray-400 pr-1'>Have an account?</span>
+                        <Link to="/">Login</Link>
+                    </div>
                 </Form>
             </div>
             <div className='col-span-4'>
-                <Carousel>
-                    <div></div>
-                </Carousel>
+                <img className='object-cover' src={imgSrc} alt="" srcset="" />
             </div>
         </div>
     )
